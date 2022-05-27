c2b5ec3ce1103e7a624721a0491c77cb2ba51d19
Jessepuc <jesse.paula@sga.pucminas.br>
2022-05-27 10:15:06 -0300

    Update IncludeProcess.js

--- src/pages/IncludeProcess.js
+++ src/pages/IncludeProcess.js
@@ -6,11 +6,15 @@ import { Link } from 'react-router-dom';
 const IncludeProcess = () => {
      return (
         <div className="flex items-center  justify-center min-h-screen bg-[url('images/themis.jpg')]">
+
             <div className="px-8 py-6 mt-4 w-9/12 min-w-max  text-left bg-white shadow-lg">
                 
                 <h3 className="text-2xl font-bold text-center"> Incluir Processo </h3>
                 <form action="">
-                <div className="mt-4 ml-6">
+
+                <div className="mt-4 grid grid-cols-2 ">
+                 <div className="mt-4 ml-6">
+                    
                      <label className="block" htmlFor="text">Texto Processo</label>
                         <input id="text-process"
                                   name="text"
@@ -180,6 +184,7 @@ const IncludeProcess = () => {
                                   autoComplete="text"
                                   required
                                   className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                 </div>
                 </div>
 
                 <div className="flex items-center justify-end ml-6">
