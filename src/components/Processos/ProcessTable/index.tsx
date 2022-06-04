4d5a10bcb9d07cd6c892b47cacb107e2f5e4cbe5
Danilo Santos <artededan@protonmail.ch>
2022-06-03 21:13:42 -0300

    (UTI Processual) Tabela de processos: Alinhamento da estilização

--- src/components/Processos/ProcessTable/index.tsx
+++ src/components/Processos/ProcessTable/index.tsx
@@ -150,39 +150,47 @@ export function ProcessTable({ processes }: IProcess) {
       }
       
       return (
-        <div > 
+        <>
+        <section className='md:container md:mx-auto px-8'>
+          <header className="bg-white">
+            <div className="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-1">
+            <section className="lg:flex lg:items-center lg:justify-between">
+              <div className="flex-1 min-w-0">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Lei de Tóxicos 11.343/06</h2>
+              </div>
+            </section>
+            </div>
+          </header>
+        </section>
+        
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
 
-            
-            <h1 className={styles.law}>Lei de Tóxicos 11.343/06</h1>
+        </Modal>
+        
+        <hr className={styles.line}/>
+        <div className={styles.processPerTypeContainer}>
+          {assigneeTypes.map(({ name, value }) => {
+            return (
+            <div key={value} className={styles.processPerType}>
+              <h3>{name}</h3>      
+              {promusNumber.map((process) => {
+                return process.assigneeType == value && processBox(process);
+                })}
+            </div>
+            );
+            })}
+        </div>
+
+
+            {/*<h1 className={styles.law}>Lei de Tóxicos 11.343/06</h1> */}
                
-              <input className={styles.searchBox}
+              {/* <input className={styles.searchBox}
               type="text" 
               placeholder="Search"
               onChange={event => {setSearchNumber(event.target.value)
               }}
-              />
+              /> */}             
 
-            <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-              
-            </Modal>
-                    
-          <hr className={styles.line}/>
-        
-            <div className={styles.processPerTypeContainer}>
-              {assigneeTypes.map(({ name, value }) => {
-                return (
-                  <div key={value} className={styles.processPerType}>
-                    <h3>{name}</h3>
-                    
-                    {promusNumber.map((process) => {
-                      return process.assigneeType == value && processBox(process);
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-                     
-        </div>  
-        
+        </> 
       );
 }
