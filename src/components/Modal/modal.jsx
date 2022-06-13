0779169068eedc2aca8e33eae3467b47fa132009
João Pedro Gomes Ferreira <jppromus@gmail.com>
2022-06-13 19:39:30 -0300

    (UTIP) modal.jsx: alteração pag infoprocessos na mesma aba.

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -71,7 +71,7 @@ function Modal ({open, children, onClose}) {
             <Link to="/infoprocesso"
                 className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-zinc-600 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500"
                 type="button"
-                target="_blank"
+                
             >
                 Mais informações
             </Link>
