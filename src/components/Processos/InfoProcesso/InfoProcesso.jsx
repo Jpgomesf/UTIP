46a05c5fc5d9f17f7a13c030ae0bb6a841ed02bf
João Pedro Gomes Ferreira <jppromus@gmail.com>
2022-06-13 18:48:27 -0300

    .

--- src/components/Processos/InfoProcesso/InfoProcesso.jsx
+++ src/components/Processos/InfoProcesso/InfoProcesso.jsx
@@ -17,7 +17,7 @@ const InfoProcesso = () => {
             </div>
             
             <span className="sm:ml-3">
-              <Link to="/processo"
+              <Link to="/dashboard"
               type="button"
               className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-zinc-600 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500"
               >
@@ -35,13 +35,13 @@ const InfoProcesso = () => {
         <section className="max-w-7xl mx-auto py-4 px-6 sm:px-6 lg:px-1">           
             <div className="bg-white shadow overflow-hidden sm:rounded-lg">
               <div className="px-4 py-5 sm:px-6">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">Dados da pessoa acusada</h3>           
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Dados do réu</h3>           
               </div>             
               
               <div className="border-t border-gray-200">
                 <dl>
                   <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-500">Nome da pessoa acusada</dt>
+                    <dt className="text-sm font-medium text-gray-500">Nome do réu</dt>
                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Igor Felipe Goncalves Magalhaes</dd>
                   </div>
 
