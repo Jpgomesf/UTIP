4c10f3874af540228fb2aa51b116f2c7b9ed9018
João Pedro Gomes Ferreira <jppromus@gmail.com>
2022-06-13 20:15:00 -0300

    revertendo pra versao anterior

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import AppRoutes from './AppRoutes';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AppRoutes />
-  </React.StrictMode>
-)
+ReactDOM.render(
+  
+    <React.StrictMode>
+      <AppRoutes />
+    </React.StrictMode>,
 
+    document.getElementById('root')
+  )
+  
 
 
 // If you want to start measuring performance in your app, pass a function
