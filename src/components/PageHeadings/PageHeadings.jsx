82d28f3799437b95a2cd3b9278edaed6a5232152
João Pedro Gomes Ferreira <jppromus@gmail.com>
2022-06-13 19:01:55 -0300

    (UTIP)index.js -> atualização do ReactDom para React18. Algumas alterações de error handling.

--- src/components/PageHeadings/PageHeadings.jsx
+++ src/components/PageHeadings/PageHeadings.jsx
@@ -25,8 +25,8 @@ export default function PageHeadings() {
             <form action="" className="relative">
               <input type="search" 
                   className="peer cursor-pointer relative z-10 h-10 w-10 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-zinc-600 focus:pl-16 focus:pr-4" placeholder="Pesquisar processo" />
-                  <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-zinc-600 px-3.5 peer-focus:border-zinc-300 peer-focus:zinc-indigo-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent -zinc-600 px-3.5 peer-focus:border-zinc-300 peer-focus:zinc-indigo-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">stroke
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                   </svg>
             </form>
           </span>
--- src/components/Processos/ProcessTable/index.tsx
+++ src/components/Processos/ProcessTable/index.tsx
@@ -165,8 +165,8 @@ export function ProcessTable({ processes }: IProcess) {
                     className="peer cursor-pointer relative z-10 h-10 w-10 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-zinc-600 focus:pl-16 focus:pr-4" placeholder="Pesquisar processo" 
                     onChange={event => {setSearchNumber(event.target.value)
                   }} />
-                  <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-zinc-600 px-3.5 peer-focus:border-zinc-300 peer-focus:zinc-indigo-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-zinc-600 px-3.5 peer-focus:border-zinc-300 peer-focus:zinc-indigo-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                   </svg>
                   </form>
                 </span>
